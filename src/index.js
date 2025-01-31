fe13f779613409da1c700d0b0480bab65dfaaeed
eanp <awifamz@gmail.com>
2025-01-31 20:17:34 +0700

    fix path

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("layout", "layouts");
 app.use(expressLayouts);
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
-app.use("/static", express.static("static", {
+app.use("/static", express.static(path.join(__dirname, "..", "static"), {
   maxAge: "1000"
 }));
-app.use("/uploads", express.static("uploads", {
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads"), {
   maxAge: "1000"
 }));
 
@@ -33,7 +33,6 @@ app.use("/", route);
 app.use((req, res, next) => {
   res.status(404).send("Sorry, page not found | 404");
 });
-
 
 app.listen(PORT, () =>
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
